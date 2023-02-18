3e20c8b74e08b012a1469291b8bc4e293f927d0e
xu.pengfei <1508075252@qq.com>
2023-02-18 16:31:22 +0800

    feat: base-测试代码分别放到各自的包里

---
+++ src/test/java/com/faber/store/StoreFileTest.java
@@ -0,0 +1,26 @@
+package com.faber.store;
+
+import com.faber.FaTestApp;
+import com.faber.api.disk.store.biz.StoreFileBiz;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {FaTestApp.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StoreFileTest {
+
+    @Resource
+    StoreFileBiz storeFileBiz;
+
+    @Test
+    public void testSyncDir() {
+        storeFileBiz.syncDir(0);
+    }
+
+}
