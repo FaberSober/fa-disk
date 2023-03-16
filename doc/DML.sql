d66e3a990aaad27eccb284f805b1db91dba2cd09
xu.pengfei <1508075252@qq.com>
2023-03-16 22:16:38 +0800

    feat: base-修改数据库升级逻辑

--- doc/DML.sql
+++ doc/DML.sql
@@ -1,2 +1,2 @@
 -- disk_store_file add info
-ALTER TABLE `file_web`.`disk_store_file` ADD COLUMN `info` varchar(255) NULL COMMENT '文件信息' AFTER `tags`;
+ALTER TABLE `disk_store_file` ADD COLUMN `info` varchar(255) NULL COMMENT '文件信息' AFTER `tags`;
