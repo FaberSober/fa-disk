0339f43e430cbd5ec04c923e9d0ba0f16a934748
xu.pengfei <1508075252@qq.com>
2023-03-16 21:26:08 +0800

    feat: base-修改数据库升级逻辑

--- src/main/java/com/faber/api/disk/FaDiskDbInit.java
+++ src/main/java/com/faber/api/disk/FaDiskDbInit.java
@@ -1,13 +1,8 @@
-package com.faber.api.disk.store;
+package com.faber.api.disk;
 
 import com.faber.core.config.dbinit.DbInit;
-import com.faber.core.config.dbinit.vo.FaDdl;
-import com.faber.core.config.dbinit.vo.FaDdlSql;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Farando
@@ -28,17 +23,4 @@ public class FaDiskDbInit implements DbInit {
         return "网盘模块";
     }
 
-    @Override
-    public List<FaDdl> getDdlList() {
-        List<FaDdl> list = new ArrayList<>();
-
-        list.add(new FaDdl(100_000_000L, "V1.0.0", "初始化")
-                .addSql(new FaDdlSql("初始化", "sql/1.0.0_disk_full.sql"))
-                .addSql(new FaDdlSql("初始化data", "sql/1.0.0_disk_data.sql")));
-
-        list.add(new FaDdl(100_000_001L, "V1.0.1", "新增文档历史记录表")
-                .addSql(new FaDdlSql("初始化", "sql/1.0.1_disk_ddl.sql")));
-
-        return list;
-    }
 }
--- src/main/resources/sql/fa-disk/1.0.0_disk_ddl.sql
+++ src/main/resources/sql/fa-disk/1.0.0_disk_ddl.sql
@@ -1,18 +1,7 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : localhost-dev-root
- Source Server Type    : MySQL
- Source Server Version : 50740
- Source Host           : localhost:3306
- Source Schema         : file_web
-
- Target Server Type    : MySQL
- Target Server Version : 50740
- File Encoding         : 65001
-
- Date: 20/02/2023 11:32:11
-*/
+-- ------------------------- info -------------------------
+-- @@ver: 1_000_000
+-- @@info: 初始化fa-disk模块
+-- ------------------------- info -------------------------
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
@@ -138,6 +127,15 @@ CREATE TABLE IF NOT EXISTS `disk_store_tag` (
 -- ----------------------------
 BEGIN;
 INSERT INTO `base_job` (`id`, `job_name`, `cron`, `status`, `clazz_path`, `job_desc`, `crt_time`, `crt_user`, `crt_name`, `crt_host`, `upd_time`, `upd_user`, `upd_name`, `upd_host`, `deleted`) VALUES (2, '[网盘][文件库]同步文件库信息', '0 0/1 * * * ?', 1, 'com.faber.api.disk.store.jobs.JobSyncBucketInfo', NULL, '2022-12-30 16:33:47', '1', '超级管理员', '127.0.0.1', '2022-12-30 16:34:10', '1', '超级管理员', '127.0.0.1', 0);
+COMMIT;
+
+-- ----------------------------
+-- Records of base_rbac_menu
+-- ----------------------------
+BEGIN;
+INSERT INTO `base_rbac_menu` (`id`, `parent_id`, `name`, `sort`, `level`, `icon`, `status`, `link_type`, `link_url`, `crt_time`, `crt_user`, `crt_name`, `crt_host`, `upd_time`, `upd_user`, `upd_name`, `upd_host`, `deleted`) VALUES (22000000, 0, '文档管理', 2, 0, 'floppy-disk', 1, 1, '/admin/disk', '2022-12-22 09:48:01', '1', '超级管理员', '127.0.0.1', NULL, NULL, NULL, NULL, 0);
+INSERT INTO `base_rbac_menu` (`id`, `parent_id`, `name`, `sort`, `level`, `icon`, `status`, `link_type`, `link_url`, `crt_time`, `crt_user`, `crt_name`, `crt_host`, `upd_time`, `upd_user`, `upd_name`, `upd_host`, `deleted`) VALUES (22010000, 22000000, '文件仓库', 0, 1, 'store', 1, 1, '/admin/disk/buckets', '2022-12-23 10:23:29', '1', '超级管理员', '127.0.0.1', NULL, NULL, NULL, NULL, 0);
+INSERT INTO `base_rbac_menu` (`id`, `parent_id`, `name`, `sort`, `level`, `icon`, `status`, `link_type`, `link_url`, `crt_time`, `crt_user`, `crt_name`, `crt_host`, `upd_time`, `upd_user`, `upd_name`, `upd_host`, `deleted`) VALUES (22020000, 22000000, '文件网盘', 1, 1, 'laptop-file', 1, 1, '/admin/disk/store', '2022-12-22 09:49:00', '1', '超级管理员', '127.0.0.1', NULL, NULL, NULL, NULL, 0);
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
