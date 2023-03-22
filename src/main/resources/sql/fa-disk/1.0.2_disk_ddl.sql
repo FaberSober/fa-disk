5a3d2ddaf09b2d4c886fa9b70bca85201d7c103c
xu.pengfei <1508075252@qq.com>
2023-03-22 22:34:28 +0800

    feat: base-docker-打包、运行

--- src/main/resources/sql/fa-disk/1.0.2_disk_ddl.sql
+++ src/main/resources/sql/fa-disk/1.0.2_disk_ddl.sql
@@ -6,8 +6,8 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS=0;
 
-ALTER TABLE `std_editor`.`disk_store_file_his` ADD COLUMN `change_file_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'Office文件变更内容zip包文件ID（适用于onlyoffice）' AFTER `file_save_id`;
+ALTER TABLE `disk_store_file_his` ADD COLUMN `change_file_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'Office文件变更内容zip包文件ID（适用于onlyoffice）' AFTER `file_save_id`;
 
-ALTER TABLE `std_editor`.`disk_store_file_his` ADD COLUMN `ver` int(11) NOT NULL COMMENT '版本号' AFTER `file_name`;
+ALTER TABLE `disk_store_file_his` ADD COLUMN `ver` int(11) NOT NULL COMMENT '版本号' AFTER `file_name`;
 
 SET FOREIGN_KEY_CHECKS=1;
