f935804d4121a15c18d037696d632d8c5e583886
xu.pengfei <1508075252@qq.com>
2025-06-13 17:37:20 +0800

    base-update junit5

--- src/test/java/com/faber/store/StoreFileTest.java
+++ src/test/java/com/faber/store/StoreFileTest.java
@@ -3,15 +3,15 @@ package com.faber.store;
 import com.faber.FaTestApp;
 import com.faber.api.disk.store.biz.StoreFileBiz;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import jakarta.annotation.Resource;
 
 @Slf4j
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {FaTestApp.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StoreFileTest {
 
